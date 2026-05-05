4af677a19ba94da76d42a3046dc1dec1cf9706f3
Taki <takiyakhlef49@gmail.com>
2026-05-05 20:41:17 +0200

    screenshots

--- ThesisRenderer/main.cpp
+++ ThesisRenderer/main.cpp
@@ -78,7 +78,6 @@ bool snapEnabled = false;
 bool gPressed = false;
 float gridSize = 1.0f;
 bool useGridSnap = true;
-float gridSize = 1.0f;
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
